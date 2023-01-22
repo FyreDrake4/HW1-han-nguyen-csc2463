ecd9cbbda1b38b251debbfef0803ad3165676ce8
FyreDrake4 <44129311+FyreDrake4@users.noreply.github.com>
2023-01-22 15:28:16 -0600

    Update sketch.js

--- Example 3/sketch.js
+++ Example 3/sketch.js
@@ -6,9 +6,9 @@ function draw() {
     background(0);
     push();
     noStroke();
-    fill(255, 249, 107); //Yellow
+    fill(255, 255, 0); //Yellow
     arc(200, 200, 350, 350, -3 * QUARTER_PI, 3 * QUARTER_PI); //This is Pac-man
-    fill(216, 79, 57);
+    fill(255, 0, 0);
     arc(600, 175, 325, 325, PI, 0); //Top Half of the Ghost
     rect(437.5, 175, 325, 187.5); //Bottom Half
     fill(255);
